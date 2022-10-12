65577f5ad4e0313a95c52ae82e4004c30923694b
praveen <praveenpkbadiger@gmail.com>
2022-10-12 10:20:41 +0530

    ADDED:[Praveen] Getting first and last Name from User

--- src/main/java/com/bridgelab/greetingapp/service/GreetingService.java
+++ src/main/java/com/bridgelab/greetingapp/service/GreetingService.java
@@ -1,5 +1,6 @@
 package com.bridgelab.greetingapp.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 
@@ -9,4 +10,11 @@ public class GreetingService {
     public String getMessage(){
         return "Hello im  Praveen from bridgelab here Sending Response ";
     }
+
+    public String getGreetingMessage(String firstName,String lastName){
+        return "Hello " + firstName + " " + lastName;
+    }
+
+
+
 }
